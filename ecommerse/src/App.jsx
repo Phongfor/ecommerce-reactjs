9a096baed484e6ddfd156bc7b2e59e646c44564b
Phongfor <hoangphong2725@gmail.com>
2025-12-20 10:12:11 +0700

    them section Info

--- ecommerse/src/App.jsx
+++ ecommerse/src/App.jsx
@@ -1,4 +1,5 @@
 import HomePage from '@components/HomePage/HomePage';
+import Info from '@components/Info/info';
 
 
 
@@ -6,6 +7,7 @@ function App() {
     return (
         <>
             <HomePage/>
+            <Info/>
         </>
     );
 }
---
+++ ecommerse/src/components/Info/constants.js
@@ -0,0 +1,29 @@
+import truckIcon from '@icons/svgs/truck-icon.svg';
+import creditCard from '@icons/svgs/creditcard.svg';
+import boxIcon from '@icons/svgs/box-icon.svg';
+import chatIcon from '@icons/svgs/chat-icon.svg';
+
+const dataInfo = [
+    {
+        title: 'Fastest Shipping',
+        description: 'Order at $39 order,',
+        src: truckIcon
+    },
+    {
+        title: '100% Safe Payments',
+        description: '9 month installments',
+        src: creditCard
+    },
+    {
+        title: '14-Days Return',
+        description: 'Shop with confidence',
+        src: boxIcon
+    },
+    {
+        title: '24/7 Online Support',
+        description: 'Delivered to home',
+        src: chatIcon
+    }
+];
+
+export {dataInfo}
